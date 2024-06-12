1ccbf88bb02541b37e5e0f3924e86ee2658a4a92
Varun Sharma <varun_sharma@live.co.uk>
2024-06-12 12:52:34 -0700

    feat: add docker compose, add requirements, and increase prod readiness

--- hello-gradio.py
+++ hello-gradio.py
@@ -6,9 +6,13 @@ import os
 import uuid
 from stable_audio_tools import get_pretrained_model
 from stable_audio_tools.inference.generation import generate_diffusion_cond
+from dotenv import load_dotenv
 
+load_dotenv()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+HF_TOKEN = os.environ.get("HF_TOKEN")
 
 # Download model
 model, model_config = get_pretrained_model("stabilityai/stable-audio-open-1.0")
@@ -18,15 +22,12 @@ sample_size = model_config["sample_size"]
 model = model.to(device)
 
 
-
 def generate_audio(prompt, seconds_total=30, steps=100, cfg_scale=7):
 
     # Set up text and timing conditioning
-    conditioning = [{
-        "prompt": prompt,
-        "seconds_start": 0,
-        "seconds_total": seconds_total
-    }]
+    conditioning = [
+        {"prompt": prompt, "seconds_start": 0, "seconds_total": seconds_total}
+    ]
     print(f"Conditioning: {conditioning}")
 
     # Generate stereo audio
@@ -40,7 +41,7 @@ def generate_audio(prompt, seconds_total=30, steps=100, cfg_scale=7):
         sigma_min=0.3,
         sigma_max=500,
         sampler_type="dpmpp-3m-sde",
-        device=device
+        device=device,
     )
     print("Audio generated.")
 
@@ -49,7 +50,14 @@ def generate_audio(prompt, seconds_total=30, steps=100, cfg_scale=7):
     print("Audio rearranged.")
 
     # Peak normalize, clip, convert to int16
-    output = output.to(torch.float32).div(torch.max(torch.abs(output))).clamp(-1, 1).mul(32767).to(torch.int16).cpu()
+    output = (
+        output.to(torch.float32)
+        .div(torch.max(torch.abs(output)))
+        .clamp(-1, 1)
+        .mul(32767)
+        .to(torch.int16)
+        .cpu()
+    )
     print("Audio normalized and converted.")
 
     # Generate a unique filename for the output
@@ -71,54 +79,50 @@ demo = gr.Interface(
         gr.Textbox(label="Prompt", placeholder="Enter your text prompt here"),
         gr.Slider(0, 47, value=30, label="Duration in Seconds"),
         gr.Slider(10, 150, value=100, step=10, label="Number of Diffusion Steps"),
-        gr.Slider(1, 15, value=7, step=0.1, label="CFG Scale")
+        gr.Slider(1, 15, value=7, step=0.1, label="CFG Scale"),
     ],
     outputs=gr.Audio(type="filepath", label="Generated Audio"),
     title="Stable Audio Generator on SaladCloud",
     description="Generate variable-length stereo audio at 44.1kHz from text prompts using Stable Audio Open 1.0.",
     examples=[
-    [
-        "Create a serene soundscape of a quiet beach at sunset.",  # Text prompt
-
-        45,  # Duration in Seconds
-        100,  # Number of Diffusion Steps
-        10,  # CFG Scale
+        [
+            "Create a serene soundscape of a quiet beach at sunset.",  # Text prompt
+            45,  # Duration in Seconds
+            100,  # Number of Diffusion Steps
+            10,  # CFG Scale
+        ],
+        [
+            "Generate an energetic and bustling city street scene with distant traffic and close conversations.",  # Text prompt
+            30,  # Duration in Seconds
+            120,  # Number of Diffusion Steps
+            5,  # CFG Scale
+        ],
+        [
+            "Simulate a forest ambiance with birds chirping and wind rustling through the leaves.",  # Text prompt
+            60,  # Duration in Seconds
+            140,  # Number of Diffusion Steps
+            7.5,  # CFG Scale
+        ],
+        [
+            "Recreate a gentle rainfall with distant thunder.",  # Text prompt
+            35,  # Duration in Seconds
+            110,  # Number of Diffusion Steps
+            8,  # CFG Scale
+        ],
+        [
+            "Imagine a jazz cafe environment with soft music and ambient chatter.",  # Text prompt
+            25,  # Duration in Seconds
+            90,  # Number of Diffusion Steps
+            6,  # CFG Scale
+        ],
+        [
+            "Rock beat played in a treated studio, session drumming on an acoustic kit.",
+            30,  # Duration in Seconds
+            100,  # Number of Diffusion Steps
+            7,  # CFG Scale
+        ],
     ],
-    [
-        "Generate an energetic and bustling city street scene with distant traffic and close conversations.",  # Text prompt
-
-        30,  # Duration in Seconds
-        120,  # Number of Diffusion Steps
-        5,  # CFG Scale
-    ],
-    [
-        "Simulate a forest ambiance with birds chirping and wind rustling through the leaves.",  # Text prompt
-        60,  # Duration in Seconds
-        140,  # Number of Diffusion Steps
-        7.5,  # CFG Scale
-    ],
-    [
-        "Recreate a gentle rainfall with distant thunder.",  # Text prompt
-
-        35,  # Duration in Seconds
-        110,  # Number of Diffusion Steps
-        8,  # CFG Scale
-
-    ],
-    [
-        "Imagine a jazz cafe environment with soft music and ambient chatter.",  # Text prompt
-        25,  # Duration in Seconds
-        90,  # Number of Diffusion Steps
-        6,  # CFG Scale
-
-    ],
-    ["Rock beat played in a treated studio, session drumming on an acoustic kit.",
-        30,  # Duration in Seconds
-        100,  # Number of Diffusion Steps
-        7,  # CFG Scale
-
-    ]
-])
+)
 
 demo.launch(server_name="[::]", server_port=8000)
-#demo.launch(server_name="0.0.0.0", server_port=8000)
+# demo.launch(server_name="0.0.0.0", server_port=8000)
